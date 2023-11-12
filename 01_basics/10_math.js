d90f522b507ec616274db8dc382794e2505d45fe
shubham chamoli <97843514+shubham18102000@users.noreply.github.com>
2023-11-12 03:18:55 +0000

    add 11

--- 01_basics/10_math.js
+++ 01_basics/10_math.js
@@ -1,6 +1,13 @@
 //+++++++++++++++++++++++++math++++++++++++++++++++++++++++
 
-console.log(Math)// math is object and having many  property
+//console.log(Math)// math is object and having many  property
 
 
-console.log(Math.floor(4.777));
+//console.log(Math.max(4.777, 5, 7 , 8,9));
+console.log((Math.random()*10)+1);
+
+// 
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1))+ min)// this is important 
