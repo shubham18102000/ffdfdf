8c9310ca03353c81b21af405a941593a7e524dbc
shubham chamoli <97843514+shubham18102000@users.noreply.github.com>
2023-11-14 03:36:24 +0000

    add date date

--- 01_basics/11_date.js
+++ 01_basics/11_date.js
@@ -6,17 +6,25 @@
 
 //*****************************Date */
 
-let myDate = new Date()
+//let myDate = new Date()
 // date is object in javascript when typeof
-console.log(myDate.toLocaleString());
+//console.log(myDate.toLocaleString());
 // comment output 2023-11-14T03:03:04.592Z
 
 // comment of localstring 11/14/2023, 3:03:59 AM
 
-console.log(typeof myDate)
+//console.log(typeof myDate)
 
-let date = new Date(2023, 0, 23, 5, 3)// in javascript the month start from 0 
+let date = new Date("01-14-2023")// in javascript the month start from 0 mm-dd-yy
 
-console.log(date.toLocaleString());
+//console.log(date.toLocaleString());
 
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(date.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDate);
 
