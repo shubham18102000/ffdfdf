d002a03ceb9000c0f00b20ef280d76fa5786bf3b
shubham chamoli <97843514+shubham18102000@users.noreply.github.com>
2023-11-15 03:40:19 +0000

    for loop in array start

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,5 +1,7 @@
 //*****************************Array*******************************//
 // javascript array will be resizable
 const myarray = [0, 1, 2, 3, 4, 5,6]
-
-console.log(myarray[0]);
+for (i=0; i<6; i++)
+{
+console.log(myarray[i]);
+}
