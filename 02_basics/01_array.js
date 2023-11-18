d79996cc6aa6f202869cf5d80ba1cef5186189d1
shubham chamoli <97843514+shubham18102000@users.noreply.github.com>
2023-11-18 11:20:36 +0000

    array day 2 splice and slice

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,13 +1,31 @@
 //*****************************Array*******************************//
 // javascript array will be resizable
 const myarray = [0, 1, 2, 3, 4, 5,6]
-for (i=0; i<6; i++)
-{
-console.log(myarray[i]);
-}
+
 // deep copy 
 
 // array method 
 // push add element in the array 
 //myarray.push(9);
 //console.log(myarray);
+//myarray.shift(5)
+//console.log(myarray)
+
+//console.log(myarray.includes(7))// boolean type answer is shown
+ //adding two Array
+
+ //const newArr = myarray.join()
+ //console.log(myarray);
+
+ //console.log(typeof newArr);
+
+ ///////////*********************** slice , splice */in javascript
+
+
+ const myn1 = myarray.slice( 1, 3) 
+ console.log(myn1)
+ // output 1,2,3
+console.log(n)
+ const myn2 = myarray.splice(1, 3)
+ console.log(myn2)
+ // output 1, 2
