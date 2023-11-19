bdacd18e7c5ac0e7f430373bf28a032cf743881c
shubham chamoli <97843514+shubham18102000@users.noreply.github.com>
2023-11-19 06:33:14 +0000

    symbol

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -4,15 +4,24 @@
 // object literals
 //object.create //constructor method
 
+// symbol decleration in javascript *****************............../
+const mySymbol = Symbol("key")
+
+
 // object literals
 const JsUser = {
    // name = key , sHUBHAM = value
     name:"shubham",
     age:18,
+    ["mysymbol"] :"chamoli",// symbol decleration inside the object
     location: "Jaipure",
     email:"shubham@gmail.com",
     isLoggedIn: false 
 }
 // how to access above value 
+// one way to call objec
 console.log(JsUser.email)
 // output= shubham@gmail.com
+// another way to call object is
+console.log(JsUser["email"])
+console.log(JsUser.mysymbol)
