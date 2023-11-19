7ff064aec4cc11143da98c8ec990eb07a2f91189
shubham chamoli <97843514+shubham18102000@users.noreply.github.com>
2023-11-19 06:22:50 +0000

    object :-19/11/2023

---
+++ 02_basics/03_object.js
@@ -0,0 +1,18 @@
+/******************object ********************* */
+// singleton 
+
+// object literals
+//object.create //constructor method
+
+// object literals
+const JsUser = {
+   // name = key , sHUBHAM = value
+    name:"shubham",
+    age:18,
+    location: "Jaipure",
+    email:"shubham@gmail.com",
+    isLoggedIn: false 
+}
+// how to access above value 
+console.log(JsUser.email)
+// output= shubham@gmail.com
