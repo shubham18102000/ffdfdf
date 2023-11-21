5968cbd8190e890effcde5d07044be293c4f8eff
shubham chamoli <97843514+shubham18102000@users.noreply.github.com>
2023-11-21 14:36:51 +0000

    function

---
+++ 02_basics/05_object3.js
@@ -0,0 +1,12 @@
+// ///*************object assign */
+
+// const obj1 = { 1: "a" , 2: "b"}
+// const obj2 = { 3: "a", 4: "b"}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+//onsole.log(obj3)
+
+
+//************************** json api */
+
+
