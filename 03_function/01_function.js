0a80fc7f91e69d25b1e9ac2abc22b4c1b3d61a67
shubham chamoli <97843514+shubham18102000@users.noreply.github.com>
2023-11-21 15:05:39 +0000

    function

--- 03_function/01_function.js
+++ 03_function/01_function.js
@@ -1,18 +1,25 @@
-// function shubham()
+// // function shubham()
+// // {
+// //     console.log(" this is me");
+// // }
+
+// // shubham();
+// // // shubham is referenc 
+
+// // function is block of code call when it is called 
+
+// function addtwonumber(num1 , num2)// parameter
 // {
-//     console.log(" this is me");
-// }
-
-// shubham();
-// // shubham is referenc 
-
-// function is block of code call when it is called 
-
-function addtwonumber(num1 , num2)// parameter
-{
     
-    console.log(num1+num2);
-} 
+//     console.log(num1+num2);
+// } 
 
-addtwonumber(5,null);// this is argument
- 
+// addtwonumber(5,null);// this is argument
+
+
+
+function loginuser(username)
+{
+    return `${username} just logged in`
+}
+console.log(loginuser("shubham"))
